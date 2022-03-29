e86f5a3fc39acb4350e441ae9016371647d43e89
Michael Mullany <mamullany@gmail.com>
2022-03-29 16:11:43 +0100

    Add functionality for clicking a button

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const availableKeys = document.getElementsByClassName("drumKey");
+/* Functionality for clicking */
+/* Get a list of the div elements that represent each drum */
+const drums = document.getElementsByClassName("drum");
+
+/* Set variable for the original background color for drum divs, so can return after flash */
+const originalBackgroundColor = drums[0].style.backgroundColor;
+
+/* Add an event listener to each that plays a child audio element when clicked */
+for (let i = 0; i < drums.length; i++) {
+    drums[i].addEventListener("click", () => {
+        playSound(drums[i]);
+    });
+}
 
 /* Functionality for pressing a key */
+/* Get a list of valid keys that can be pressed to make a sound */
+const availableKeys = document.getElementsByClassName("drumKey");
+
 document.addEventListener("keydown", (event) => {
     pressedKey = event.key.toUpperCase();
     selectedElement = selectElementFromKeyPress(pressedKey);
@@ -21,8 +36,17 @@ const selectElementFromKeyPress = (pressedKey) => {
     }
 }
 
-/* Given a "drum", find a child that is an audio element and play it */
+/* Given a "drum", find a child audio element and play it */
 const playSound = drum => {
+    flashDrum(drum, "green", 500);
     let sound = drum.getElementsByTagName("audio")[0];
     sound.play();
+    setTimeout()
+}
+
+function flashDrum(drum, color, flashLength) {
+    drum.style.backgroundColor = color;
+    setTimeout(() => {
+        drum.style.backgroundColor = originalBackgroundColor;
+    }, flashLength);
 }
